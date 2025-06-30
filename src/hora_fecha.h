e06e90f20b9534c8255210e317b7ed0a5b856762
Marco50345 <00114825@uca.edu.sv>
2025-06-29 22:29:38 -0600

    Agregamos un estilo para mostrar la fecha y la hora y traduccimos el codigo

--- src/hora_fecha.h
+++ src/hora_fecha.h
@@ -1,28 +1,55 @@
-#ifndef HORA_FECHA_H
-#define HORA_FECHA_H
-
-#include <iostream>
+#include <iostream> 
 #include <ctime>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 using namespace std;
 
-void mostrarFechaHora() {
-    time_t ahora = time(0);
-    tm* tiempoLocal = localtime(&ahora);
+void showDateTime()
+{
+    time_t now = time(0);
+    tm *localTime = localTime(&now);
+    cout << "╔════════════════════════════════════╗\n";
+    cout << "║ Date :";
 
-    cout << "\nFecha y hora actual: ";
-    cout << tiempoLocal->tm_mday << "/"
-         << tiempoLocal->tm_mon + 1 << "/"
-         << tiempoLocal->tm_year + 1900 << " ";
+    cout << localTime->tm_mday << "/"
+         << localTime->tm_mon + 1 << "/"
+         << localTime->tm_year + 1900 << "                   ║";
+    cout << "\n║ Time : ";
 
-    if (tiempoLocal->tm_hour < 10) cout << "0";
-    cout << tiempoLocal->tm_hour << ":";
-
-    if (tiempoLocal->tm_min < 10) cout << "0";
-    cout << tiempoLocal->tm_min << ":";
-
-    if (tiempoLocal->tm_sec < 10) cout << "0";
-    cout << tiempoLocal->tm_sec << endl;
+    if (localTime->tm_hour < 10)
+        cout << "0";
+    cout << localTime->tm_hour << ":";
+    if (localTime->tm_min < 10)
+        cout << "0";
+    cout << localTime->tm_min << ":";
+    if (localTime->tm_sec < 10)
+        cout << "0";
+    cout << localTime->tm_sec << "                    ║" << endl;
+    cout << "╚════════════════════════════════════╝\n";
 }
 
-#endif // HORA_FECHA_H
+int main()
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    while (true)
+    {
+#ifdef _WIN32
+        system("cls");
+#else
+        system("clear");
+#endif
+        showDateTime();
+#ifdef _WIN32
+        Sleeep(1000);
+#else
+        sleep(1);
+#endif
+    }
+
+    return 0;
+}
